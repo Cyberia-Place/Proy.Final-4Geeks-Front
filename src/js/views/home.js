9f71294ab21e068b23f5bb4839adfb33632ed23b
analiabelo98 <analiabelo.42@gmail.com>
2021-06-03 12:44:20 +0000

    Ajustes del perfil fix

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,14 +3,11 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import Informacion_form from "../component/informacion_del_perfil";
 import Contraseña_form from "../component/contraseña";
-import Pago_form from "../component/Información_pago";
+import Pago_form from "../component/Informacion_pago";
 import Cambiar_foto from "../component/cambiar_foto";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<Cambiar_foto />
-		<Informacion_form />
-		<Contraseña_form />
-		<Pago_form />
+		
 	</div>
 );
