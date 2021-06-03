95e31c4614ead62f06da2f9c94c6c0d71d6bf4d1
analiabelo98 <analiabelo.42@gmail.com>
2021-06-03 12:43:14 +0000

    Ajustes del perfil

---
+++ src/js/component/informacion_del_perfil.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+	textarea: {
+		resize: "both"
+	}
+}));
+
+export default function Informacion_form() {
+	const classes = useStyles();
+	return (
+		<React.Fragment>
+			<Typography variant="h6" aling="left" className="mt-3" gutterBottom>
+				Información del perfil
+			</Typography>
+			<Grid container spacing={3} md={6} justify="center" className="m-auto">
+				<Grid item xs={12} sm={6} md={6}>
+					<TextField
+						required
+						id="firstName"
+						name="firstName"
+						label="Nombre completo:"
+						fullWidth
+						autoComplete="given-name"
+					/>
+				</Grid>
+				<Grid item xs={12} sm={6} md={6}>
+					<TextField required id="age" name="age" label="Edad:" fullWidth autoComplete="age" />
+				</Grid>
+				<Grid item xs={12} sm={6} md={6}>
+					<TextField
+						required
+						id="email"
+						name="email"
+						label="Correo electronico:"
+						fullWidth
+						autoComplete="email"
+					/>
+				</Grid>
+				<Grid item xs={12} sm={6} md={6}>
+					<TextField
+						required
+						id="language"
+						name="language"
+						label="idioma:"
+						fullWidth
+						autoComplete="language"
+					/>
+				</Grid>
+				<Grid item xs={12} sm={6} md={6}>
+					<TextField required id="city" name="city" label="País" fullWidth autoComplete="address" />
+				</Grid>
+				<Grid item xs={12} sm={6} md={6}>
+					<TextField required id="ocupacion" name="ocupacion" label="Ocupacion:" fullWidth />
+				</Grid>
+				<Grid item xs={12} sm={6} md={6}>
+					<TextField
+						id="outlined-textarea"
+						label="Descripcion:"
+						placeholder="Descripcion:"
+						multiline
+						fullWidth
+						rows={6}
+						variant="outlined"
+						inputProps={{ className: classes.textarea }}
+					/>
+				</Grid>
+			</Grid>
+
+			<Button className="m-5">Guardar</Button>
+			<Divider variant="middle" />
+		</React.Fragment>
+	);
+}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,16 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import Informacion_form from "../component/informacion_del_perfil";
+import Contraseña_form from "../component/contraseña";
+import Pago_form from "../component/Información_pago";
+import Cambiar_foto from "../component/cambiar_foto";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<Cambiar_foto />
+		<Informacion_form />
+		<Contraseña_form />
+		<Pago_form />
 	</div>
 );
