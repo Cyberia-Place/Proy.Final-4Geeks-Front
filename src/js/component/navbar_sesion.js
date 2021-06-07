9b41db6f8b6eafd84e6e2b80d23b0e8f6d51dfc3
Cyberia-Place <eliasberto2@gmail.com>
2021-06-07 23:19:55 +0000

    subo tema global

--- src/js/component/navbar_sesion.js
+++ src/js/component/navbar_sesion.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Toolbar from "@material-ui/core/Toolbar";
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
@@ -10,6 +10,9 @@ import Badge from "@material-ui/core/Badge";
 import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MoreIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles(theme => ({
 	navBar: {
@@ -61,11 +64,95 @@ const useStyles = makeStyles(theme => ({
 		[theme.breakpoints.up("md")]: {
 			display: "flex"
 		}
+	},
+	sectionMobile: {
+		display: "flex",
+		[theme.breakpoints.up("md")]: {
+			display: "none"
+		}
 	}
 }));
 
 export const NavbarSesion = () => {
 	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+	const isMenuOpen = Boolean(anchorEl);
+	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+	// Handlers para la vista de escritorio
+	const handleProfileMenuOpen = event => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleMenuClose = () => {
+		setAnchorEl(null);
+		handleMobileMenuClose();
+	};
+
+	// Handlers para la vista de celular
+	const handleMobileMenuClose = () => {
+		setMobileMoreAnchorEl(null);
+	};
+	const handleMobileMenuOpen = event => {
+		setMobileMoreAnchorEl(event.currentTarget);
+	};
+
+	// Menu de la vista de escritorio
+	const menuId = "primary-search-account-menu";
+	const renderMenu = (
+		<Menu
+			anchorEl={anchorEl}
+			anchorOrigin={{ vertical: "top", horizontal: "right" }}
+			id={menuId}
+			keepMounted
+			transformOrigin={{ vertical: "top", horizontal: "right" }}
+			open={isMenuOpen}
+			onClose={handleMenuClose}>
+			<MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+			<MenuItem onClick={handleMenuClose}>My account</MenuItem>
+		</Menu>
+	);
+
+	// Menu de la vista de celular
+	const mobileMenuId = "primary-search-account-menu-mobile";
+	const renderMobileMenu = (
+		<Menu
+			anchorEl={mobileMoreAnchorEl}
+			anchorOrigin={{ vertical: "top", horizontal: "right" }}
+			id={mobileMenuId}
+			keepMounted
+			transformOrigin={{ vertical: "top", horizontal: "right" }}
+			open={isMobileMenuOpen}
+			onClose={handleMobileMenuClose}>
+			<MenuItem>
+				<IconButton aria-label="show 4 new mails" color="inherit">
+					<Badge badgeContent={4} color="error">
+						<MailIcon />
+					</Badge>
+				</IconButton>
+				<p>Messages</p>
+			</MenuItem>
+			<MenuItem>
+				<IconButton aria-label="show 11 new notifications" color="inherit">
+					<Badge badgeContent={11} color="error">
+						<NotificationsIcon />
+					</Badge>
+				</IconButton>
+				<p>Notifications</p>
+			</MenuItem>
+			<MenuItem onClick={handleProfileMenuOpen}>
+				<IconButton
+					aria-label="account of current user"
+					aria-controls="primary-search-account-menu"
+					aria-haspopup="true"
+					color="inherit">
+					<AccountCircle />
+				</IconButton>
+				<p>Profile</p>
+			</MenuItem>
+		</Menu>
+	);
 
 	return (
 		<div>
@@ -91,27 +178,39 @@ export const NavbarSesion = () => {
 					<div className={classes.searchDiv} />
 					<div className={classes.sectionDesktop}>
 						<IconButton aria-label="show 4 new mails" color="inherit">
-							<Badge badgeContent={4} color="secondary">
+							<Badge badgeContent={4} color="error">
 								<MailIcon />
 							</Badge>
 						</IconButton>
 						<IconButton aria-label="show 17 new notifications" color="inherit">
-							<Badge badgeContent={17} color="secondary">
+							<Badge badgeContent={17} color="error">
 								<NotificationsIcon />
 							</Badge>
 						</IconButton>
 						<IconButton
 							edge="end"
 							aria-label="account of current user"
-							// aria-controls={menuId}
+							aria-controls={menuId}
 							aria-haspopup="true"
-							// onClick={handleProfileMenuOpen}
+							onClick={handleProfileMenuOpen}
 							color="inherit">
 							<AccountCircle />
 						</IconButton>
 					</div>
+					<div className={classes.sectionMobile}>
+						<IconButton
+							aria-label="show more"
+							aria-controls={mobileMenuId}
+							aria-haspopup="true"
+							onClick={handleMobileMenuOpen}
+							color="inherit">
+							<MoreIcon />
+						</IconButton>
+					</div>
 				</Toolbar>
 			</AppBar>
+			{renderMobileMenu}
+			{renderMenu}
 		</div>
 	);
 };
--- src/js/layout.js
+++ src/js/layout.js
@@ -8,6 +8,21 @@ import { Single } from "./views/single";
 import { InicioAlumno } from "./views/index_usuario_logueado";
 import injectContext from "./store/appContext";
 import { Footer } from "./component/footer";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: "#59a80f"
+		},
+		secondary: {
+			main: "#aef060"
+		},
+		error: {
+			main: "#fdd835"
+		}
+	}
+});
 
 //create your first component
 const Layout = () => {
@@ -16,30 +31,32 @@ const Layout = () => {
 	const basename = process.env.BASENAME || "";
 
 	return (
-		<div className="d-flex flex-column">
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Switch>
-						<Route exact path="/">
-							<Home />
-						</Route>
-						<Route exact path="/demo">
-							<Demo />
-						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
-						</Route>
-						<Route exact path="/inicio/alumno">
-							<InicioAlumno />
-						</Route>
-						<Route>
-							<h1>Not found!</h1>
-						</Route>
-					</Switch>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
+		<ThemeProvider theme={theme}>
+			<div className="d-flex flex-column">
+				<BrowserRouter basename={basename}>
+					<ScrollToTop>
+						<Switch>
+							<Route exact path="/">
+								<Home />
+							</Route>
+							<Route exact path="/demo">
+								<Demo />
+							</Route>
+							<Route exact path="/single/:theid">
+								<Single />
+							</Route>
+							<Route exact path="/inicio/alumno">
+								<InicioAlumno />
+							</Route>
+							<Route>
+								<h1>Not found!</h1>
+							</Route>
+						</Switch>
+						<Footer />
+					</ScrollToTop>
+				</BrowserRouter>
+			</div>
+		</ThemeProvider>
 	);
 };
 
