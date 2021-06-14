5e62267dbed391fb21a5b1b1bc8017bdecffa888
Cyberia-Place <eliasberto2@gmail.com>
2021-06-14 22:57:06 +0000

    subo boton funcional google

--- src/js/component/boton_google.js
+++ src/js/component/boton_google.js
@@ -3,7 +3,7 @@ import GoogleLogin from "react-google-login";
 
 const responseGoogle = response => {
 	console.log(response);
-	console.log(response);
+	console.log(response.profileObj);
 };
 
 export const BotonGoogle = () => {
