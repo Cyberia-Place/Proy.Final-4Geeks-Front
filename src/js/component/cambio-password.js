28879a64cb03afa5203513f16dfd5602830be005
Cyberia-Place <eliasberto2@gmail.com>
2021-06-17 02:01:50 +0000

    casi hecho

--- src/js/component/cambio-password.js
+++ src/js/component/cambio-password.js
@@ -1,37 +1,43 @@
 import React from "react";
-import { Grid, OutlinedInput, InputLabel, makeStyles } from "@material-ui/core";
+import { Grid, InputLabel, makeStyles, Typography, OutlinedInput } from "@material-ui/core";
 
-// const useStyles = makeStyles({
-// 	form: {}
-// });
+const useStyles = makeStyles({
+	input: {
+		width: "10vw"
+	}
+});
 
 export const CambioPassword = () => {
-	// const classes = useStyles();
+	const classes = useStyles();
 
 	return (
 		<div style={{ backgroundColor: "#252525" }} className="d-flex justify-content-center h-100">
 			<div
-				className="p-5 bg-light d-flex align-items-center"
+				className="p-5 bg-light"
 				style={{
 					width: "50vw",
 					height: "100%",
 					borderLeft: "4px solid #59a80f",
 					borderRight: "4px solid #59a80f"
 				}}>
-				<Grid container direction="column" justify="center" alignItems="center" spacing={6}>
-					<Grid item xs={12}>
+				<Typography variant="h3" align="center" gutterBottom>
+					Cambio de contraseña
+				</Typography>
+				<br />
+				<div style={{ width: "70%", height: "100%" }} className="mx-auto">
+					<div style={{ width: "100%", marginBottom: "30px" }}>
 						<InputLabel>Antigua contraseña</InputLabel>
-						<OutlinedInput variant="outlined" color="secondary" />
-					</Grid>
-					<Grid item xs={12}>
+						<OutlinedInput color="primary" fullWidth={true} />
+					</div>
+					<div style={{ width: "100%", marginBottom: "30px" }}>
 						<InputLabel>Nueva contraseña</InputLabel>
-						<OutlinedInput variant="outlined" color="secondary" />
-					</Grid>
-					<Grid item xs={12}>
+						<OutlinedInput color="primary" fullWidth={true} />
+					</div>
+					<div style={{ width: "100%", marginBottom: "30px" }}>
 						<InputLabel>Repita nueva contraseña</InputLabel>
-						<OutlinedInput variant="outlined" color="secondary" />
-					</Grid>
-				</Grid>
+						<OutlinedInput color="primary" fullWidth={true} />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
